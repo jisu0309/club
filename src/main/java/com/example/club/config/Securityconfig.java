88047de8becfe4ec24a7c7611ed82ea745e32088
jisu0309 <sksfh0147@gmail.com>
2021-03-12 18:07:26 +0900

    Securityconfig

--- src/main/java/com/example/club/config/Securityconfig.java
+++ src/main/java/com/example/club/config/Securityconfig.java
@@ -1,13 +1,28 @@
 package com.example.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
 public class Securityconfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        //사용자 걔정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$peWNFhHYYNxumCj31BiF2e/MrI.w57nRDpXMKIZ8rNXVxVBrRUgem")
+                .roles("USER");
+    }
 
 }
